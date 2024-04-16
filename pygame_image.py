6683388be99089839ad9ec04a197467a7cbf1112
OtaniHonoka <c0b2302862@edu.teu.ac.jp>
2024-04-16 16:30:33 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -17,7 +17,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])#blitで貼り付け 練習3
+        x = tmr%800
+        screen.blit(bg_img, [-x, 0])#blitで貼り付け 練習3
         screen.blit(kk_img, [300, 200])#練習4
         
         pg.display.update()
