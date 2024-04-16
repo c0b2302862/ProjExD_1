a279f6040f9ae1ac194fb4acd1cfebb9f1344bc9
OtaniHonoka <c0b2302862@edu.teu.ac.jp>
2024-04-16 17:45:45 +0900

    練習8

--- pygame_image.py
+++ pygame_image.py
@@ -13,11 +13,24 @@ def main():
     kk_img = pg.image.load("fig/3.png")#練習2
     bg_img2 = pg.image.load("fig/pg_bg.jpg")#練習7
     kk_img = pg.transform.flip(kk_img, True, False)#練習2
-    bg_img2 = pg.transform.flip(bg_img2, True, False)#練習7
+    bg_img2 = pg.transform.flip(bg_img, True, False)#練習7
+    kk_rct = kk_img.get_rect() #練習8-1
+    kk_rct.center = 300, 200 #練習8-2
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            #print("上押された")
+            kk_rct.move_ip([0, -1])
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip([0, +1])
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip([-1, 0])
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip([+1, 0])
 
         x = tmr%3200 #練習6 割る数は背景画像の長さによる
         screen.blit(bg_img, [-x, 0])#blitで貼り付け 練習3 #練習6
@@ -25,11 +38,16 @@ def main():
         screen.blit(bg_img2, [-x+1600, 0])#練習7
         screen.blit(bg_img2, [-x+3200, 0])#練習7
         screen.blit(bg_img2, [-x+4800, 0])#練習7
-        screen.blit(kk_img, [300, 200])#練習4
+        # screen.blit(kk_img, [300, 200])#練習4
+
+        
+        
+        screen.blit(kk_img, kk_rct)#練習8-5
+
 
         pg.display.update()
         tmr += 1        
-        clock.tick(500)#練習5
+        clock.tick(200)#練習5
 
 
 if __name__ == "__main__":
